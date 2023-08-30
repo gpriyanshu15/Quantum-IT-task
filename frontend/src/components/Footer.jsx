b0440f0352ebbc3b6b003b67beb1c00432b2adb1
Priyanshu Gupta <92108888+gpriyanshu15@users.noreply.github.com>
2023-08-30 16:05:50 +0530

    ne

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,30 @@
+
+export default function Footer() {
+    
+    return (
+        <div style={{padding: "12px 0px", backgroundColor : "rgb(232, 227, 227)", textAlign: 'center', position:'fixed', width: '100%',height : '85px', bottom : '0', left: '0', right: '0', zIndex : '999'}}>
+            
+            <div className="text-center">
+                    <h5>
+                        Made  by <a href="https://akshata-ganbote.netlify.app/" style={{textDecoration : "none", color: "red"}}>Priyanshu Gupta</a>
+                    </h5>
+            </div> 
+
+            <div className="text-center pt-1">
+                <a href="https://www.linkedin.com/in/priyanshu-gupta-724443210" target="_blank" rel="noreferrer">
+                    <i className="bi bi-linkedin mx-2" style={{fontSize : "20px"}}></i>
+                </a>
+                
+                <a href="https://github.com/gpriyanshu15" target="_blank" rel="noreferrer">
+                    <i className="bi bi-github mx-2" style={{fontSize : "21px"}}></i>
+                </a>
+
+                <a href="mailto:priyanshufractal@gmail.com" target="_blank" rel="noreferrer">
+                    <i className="bi bi-envelope-fill mx-2" style={{fontSize : "21px"}}></i>
+                </a>
+            </div>
+           
+        </div>
+       
+    )
+}
